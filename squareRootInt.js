c7dc73aaa6c3090e0d859a8d619f6472e3876705
Saurabh Pande <srbh_91@outlook.com>
2020-12-05 17:56:29 +0530

    Update squareRootInt.js

--- squareRootInt.js
+++ squareRootInt.js
@@ -1,17 +1,13 @@
 function squareRootInt(n, g) {
     if (!g) {
-        // Take an initial guess at the square root
-        g = n / 2.0;
+        g = n / 2;
     }
-    var d = n / g;              // Divide our guess into the number
-    var ng = (d + g) / 2.0;     // Use average of g and d as our new guess
+    var d = n / g;              
+    var ng = (d + g) / 2;    
     if (g == ng) {          
-        // The new guess is the same as the old guess; further guesses
-        // can get no more accurate so we return this guess
         return g;
     }
-    // Recursively solve for closer and closer approximations of the square root
-    return squirt(n, ng);
+    return squareRootInt(n, ng);
 }
 
 console.log(squareRootInt(42)); // 6.48074069840786
